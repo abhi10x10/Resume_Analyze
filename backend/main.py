52449fbf9e1ff831d6e4cfaa10b1a6aa80620769
abhi10x10 <abhidev4451ji@gmail.com>
2026-08-07 15:49:46 +0530

    now allowed render also

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ import shutil
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://resume-analyze-fw002.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
